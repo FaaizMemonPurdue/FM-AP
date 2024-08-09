9cb0401c22e062567b241e237494bfb4b2e09e84
FaaizMemonPurdue <fmemon@purdue.edu>
2024-08-09 14:50:59 -0400

    messy

---
+++ critical_submodules.sh
@@ -0,0 +1,8 @@
+set -e
+set -x
+
+git config --global --add safe.directory /ardupilot
+
+git submodule update --init --recursive --depth 1 modules/mavlink \
+    ; git submodule update --init --recursive --depth 1 modules/DroneCAN \
+    ; git submodule update --init --recursive --depth 1 modules/waf
