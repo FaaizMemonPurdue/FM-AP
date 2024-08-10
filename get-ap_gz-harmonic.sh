377f29b40dc350fde72acc50da6fe92a7eecc4c7
FaaizMemonPurdue <fmemon@purdue.edu>
2024-08-10 02:58:04 -0400

    be clearer

--- get-ap_gz-harmonic.sh
+++ get-ap_gz-harmonic.sh
@@ -3,9 +3,10 @@ set -x
 
 cd ~
 sudo apt update
-sudo apt install libgz-sim8-dev rapidjson-dev
-sudo apt install libopencv-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl
+sudo apt install libgz-sim8-dev rapidjson-dev -y
+sudo apt install libopencv-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
 
+export GZ_VERSION=harmonic
 git clone https://github.com/ArduPilot/ardupilot_gazebo
 cd ardupilot_gazebo
 mkdir build && cd build
@@ -14,4 +15,4 @@ make -j4
 
 echo 'export GZ_SIM_SYSTEM_PLUGIN_PATH=$HOME/ardupilot_gazebo/build:${GZ_SIM_SYSTEM_PLUGIN_PATH}' >> ~/.bashrc
 echo 'export GZ_SIM_RESOURCE_PATH=$HOME/ardupilot_gazebo/models:$HOME/ardupilot_gazebo/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
-
+echo 'export GZ_VERSION=harmonic' >> ~/.bashrc
