f6ccbfb8104c56984278a5a93e5b71a4d84227d7
FaaizMemonPurdue <fmemon@purdue.edu>
2024-08-09 23:09:57 -0400

    helper gz

---
+++ get-gz-harmonic.sh
@@ -0,0 +1,12 @@
+set -e
+set -x
+
+sudo apt-get update
+sudo apt-get install lsb-release gnupg -y
+
+sudo apt-get install curl
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install gz-harmonic -y
+
