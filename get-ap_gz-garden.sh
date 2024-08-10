602769b29e1c7beb9e76129013e6b78151c86b53
FaaizMemonPurdue <fmemon@purdue.edu>
2024-08-10 12:33:36 -0400

    seems good

--- get-ap_gz-garden.sh
+++ get-ap_gz-garden.sh
@@ -3,15 +3,15 @@ set -x
 
 cd ~
 sudo apt update
-sudo apt install libgz-sim7-dev rapidjson-dev
-sudo apt install libopencv-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl
+sudo apt install libgz-sim7-dev rapidjson-dev -y
+sudo apt install libopencv-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
 
 export GZ_VERSION=garden
 git clone https://github.com/ArduPilot/ardupilot_gazebo
 cd ardupilot_gazebo
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
-make -j4
+make -j8
 
 echo 'export GZ_SIM_SYSTEM_PLUGIN_PATH=$HOME/ardupilot_gazebo/build:${GZ_SIM_SYSTEM_PLUGIN_PATH}' >> ~/.bashrc
 echo 'export GZ_SIM_RESOURCE_PATH=$HOME/ardupilot_gazebo/models:$HOME/ardupilot_gazebo/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
